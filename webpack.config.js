749c391eba6c9fafd2e1ae0f11b6f2b00e4c1128
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-06 01:13:07 -0300

    resolving webpack issues once again

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,10 @@ module.exports = {
       "path": require.resolve("path-browserify"),
       "os": require.resolve("os-browserify/browser"),
       "crypto": require.resolve("crypto-browserify"),
-      "buffer": require.resolve("buffer/")
+      "buffer": require.resolve("buffer/"),
+      "vm": require.resolve("vm-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "stream": require.resolve("stream-browserify")
     }
   }
 };
