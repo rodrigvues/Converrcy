d7ffba08fbfbe0424e0b2555827950eff00c4112
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-12 20:19:40 -0300

    fixing body and root to fill the content into web screen

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -2,17 +2,22 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
+    height: 100%;
     margin: 0;
     padding: 0;
     font-family: 'Inter', sans-serif;
     background-color: #04471C;
     color: #E0EEC6;
+    display: flex;
   }
 
   #root {
+    height: 100%;
+    width: 100%;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    padding: 20px; /* Add padding to create some space around the content */
+    box-sizing: border-box; /* Ensure padding doesn't affect the width/height calculation */
   }
 `;
 
