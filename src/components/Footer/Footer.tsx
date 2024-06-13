3ae9aeb5a3e1824afbaed4ff3bf5c501a2261747
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2024-06-12 21:49:21 -0300

    creating footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterBox } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <FooterBox>
+      
+    </FooterBox>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const FooterBox = styled.div`
+  background-color: #04471C;
+  color: #E0EEC6;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start; // Align content to the left
+  width: 100%;
+  max-width: 600px; // Ensure it doesn't stretch too wide
+  margin-top: 0px;
+  margin-left: 45px;
+  margin-right: 45px;
+`;
